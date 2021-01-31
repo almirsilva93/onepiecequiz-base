f13a88ab334b22b072f6566d7ae4a84c69de253c
Almir Silva <almir_matos@yahoo.com.br>
2021-01-31 11:20:44 -0300

    Finalizando Imersão Alura

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
 
@@ -26,15 +29,21 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+// eslint-disable-next-line prefer-destructuring
 const theme = db.theme;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
